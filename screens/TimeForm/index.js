3c04d99162585c1f1faf8cadff48678432f76c70
Anisha Jain <anishajain@outlook.com>
2020-01-06 22:15:12 -0800

    basic form content

--- screens/TimeForm/index.js
+++ screens/TimeForm/index.js
@@ -1,27 +1,62 @@
 import React, { Component } from "react";
 import {
-  Text, View, TouchableHighlight
+  View,
+  TouchableHighlight,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
+import { Text, TextInput } from 'react-native-custom-fonts';
+import styles from "../commonStyles";
 
 export default class TimeFormScreen extends Component {
   static navigationOptions = {
     header: null
   };
 
-  logout() {
-    const { navigate } = this.props.navigation;
-    navigate('Auth')
+  constructor(props) {
+    super(props);
+    this.state = {
+      budget: "",
+      time: "",
+      bank: "" // token from backend
+    };
   }
 
   render() {
     return (
-      <View style={{flex: 1, justifyContent: 'center'}}>
-        <Text>Home</Text>
-        <TouchableHighlight
-          >
-              <Text>LOG OUT</Text>
-        </TouchableHighlight>
-      </View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : null}
+        style={{ flex: 1 }}
+      >
+        <View style={styles.container}>
+          <Text style={styles.logo}>Budge</Text>
+          <Text>
+            Description to tell them to enter budget and time and how it will be
+            used... ?? Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+            sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+            aliquam erat volutpat.
+          </Text>
+          <View style={styles.form}>
+            <TextInput
+              style={styles.input}
+              onChangeText={text => this.onChangeBudget(text)}
+              placeholder="00.00"
+            />
+            <TextInput
+              style={styles.input}
+              onChangeText={text => this.onChangeTime(text)}
+              placeholder="00.00.00"
+            />
+            {/* NON functional for MVP */}
+            <TouchableHighlight onPress={() => this.connectBank()}>
+              <Text style={styles.input}>Use account to track spending</Text>
+            </TouchableHighlight>
+            <TouchableHighlight onPress={() => this.start()}>
+              <Text style={styles.button}>START</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
     );
   }
 }
---
+++ screens/commonStyles.js
@@ -0,0 +1,29 @@
+import {
+    StyleSheet
+  } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        justifyContent: "center",
+        alignItems: 'center',
+    },
+    logo: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        fontFamily: 'Khula',
+        fontWeight: 'normal',
+    },
+    form: {
+        alignItems: 'center',
+    },
+    input: {
+
+    },
+    button: {
+        
+    }
+    
+  });
+  
+export default styles;
