695d32ccce43d5d3f12fe01bb8693b75cf3342e9
Anisha Jain <anishajain@outlook.com>
2020-01-09 11:57:20 -0800

    summary boilerplate code

--- screens/Summary/index.js
+++ screens/Summary/index.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import {
   View,
-  KeyboardAvoidingView,
-  Platform,
+  TouchableHighlight,
   Text
 } from "react-native";
 import styles from "../commonStyles";
+import { _retrieveData, _removeData } from "../../helpers/store";
 
 export default class SummaryScreen extends Component {
   static navigationOptions = {
@@ -15,23 +15,61 @@ export default class SummaryScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      budget: "",
-      time: "",
-      bank: "" // token from backend
+      data: null,
     };
   }
 
+  componentDidMount() {
+    this.getStoredData()
+  }
+
+  async getStoredData() {
+    await _retrieveData("timerData").then(data => {
+      this.setState({ data: JSON.parse(data) });
+    });
+  }
+
+  async ok() {
+    this.setState({
+      data: null
+    });
+    const { navigate } = this.props.navigation;
+    // Remove data from local storage
+    await _removeData("timerData").then(async () => {
+      // Navigate back to timeForm page
+      navigate("TimeForm");
+    });
+  }
+
   render() {
+    const remaining = this.state.data
+      ? `$${this.state.data.budget - this.state.data.spent}`
+      : "loading";
+    const spent = this.state.data ? `$${this.state.data.spent}` : "loading";
+    const budget = this.state.data ? `$${this.state.data.budget}` : "loading";
+
+    const diff = this.state.data ? this.state.data.budget - this.state.data.spent : 0
+    const savedString = diff < 0 ? `You spent ${Math.abs(diff)} over budget!` : `You spent ${Math.abs(diff)} under budget`
     return (
-      <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : null}
-        style={{ flex: 1 }}
-      >
-        <View style={styles.container}>
-            <Text> Hello </Text>
+      <View style={styles.container}>
+          <Text style={styles.logo}>Budge</Text>
+          <Text style={styles.time}>Time up!</Text>
+          <View style={styles.stats}>
+            <Text style={styles.statTitle}>Remaining budget</Text>
+            <Text style={styles.statInfo}>{remaining}</Text>
+            <Text style={styles.statTitle}>Total spent so far</Text>
+            <Text style={styles.statInfo}>{spent}</Text>
+            <Text style={styles.statTitle}>Total budget</Text>
+            <Text style={styles.statInfo}>{budget}</Text>
+            <Text style={styles.summary}>
+              You spent ${savedString} under budget
+            </Text>
+          </View>
+          <TouchableHighlight onPress={() => this.ok()}>
+            <Text style={styles.button}>DONE</Text>
+          </TouchableHighlight>
+        {/* TODO: ^ ok button that clears local storage */}
         </View>
-        {/* TODO: ok button that clears local storage */}
-      </KeyboardAvoidingView>
     );
   }
 }
